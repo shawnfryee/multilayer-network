077f4d636d6ce56ece88725ec34a0ee50aa49c09
shawnfrye <shawnmichaelfrye@outloook.com>
2024-03-26 06:50:14 -0400

    added loa data method, fixed some of the training stuff, tried creating a fake datgaset forf testing but getting errors, need to debug further

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 import os
 import configparser
+import train
+import numpy as np
+from PIL import Image
+from sklearn.model_selection import train_test_split
+
 
 def make_output_directory():
     current_project_directory = os.getcwd()
@@ -11,20 +16,52 @@ def make_output_directory():
         print(f"Directory '{output_directory}' created successfully.")
     else:
         print(f"Directory '{output_directory}' already exists.")
+    return output_directory
 
+
+def load_data(image_folder, label_folder):
+    # load n sort dataset TODO needs to be expanded for subdirs for stop, go, left, right. This is just very basic
+    image_files = os.listdir(image_folder)
+    label_files = os.listdir(label_folder)
+    image_files.sort()
+    label_files.sort()
+
+    images = []
+    labels = []
+
+    # Load images and labels
+    for img_file, label_file in zip(image_files, label_files):
+        img_path = os.path.join(image_folder, img_file)
+        label_path = os.path.join(label_folder, label_file)
+
+        img = Image.open(img_path)
+        img_array = np.array(img)
+        images.append(img_array)
+
+        # TODO needs to be expanded for stop, go, left, right labels. This is just very basic
+        with open(label_path, 'r') as f:
+            label = int(f.readline().strip())  # Assuming label is a single digit
+        labels.append(label)
+
+    images = np.array(images)
+    labels = np.array(labels)
+
+    # split data into training and testing sets
+    X_train, X_test, Y_train, Y_test = train_test_split(images, labels, test_size=0.2, random_state=42)
+
+    return X_train, Y_train, X_test, Y_test
 def main():
-    make_output_directory()
-    #get config parameters
-    config_file = os.path.join(os.path.dirname(os.getcwd()), "config.ini")
-   # config = configparser.ConfigParser()
-   # config.read(config_file)
+    output_directory = make_output_directory()
+# TODO make a config file that is read to change number of epochs and different parameters for optimizing ther model
     #get dataset
     data_folder = os.path.join(os.path.dirname(os.getcwd()), "dataset/")
+    images_folder = os.path.join(data_folder, "images")
+    labels_folder = os.path.join(data_folder, "labels")
 
-
-    print(data_folder)
-    print(config_file)
     #setup training stuff
+    X_train, Y_train, X_test, Y_test = load_data(images_folder, labels_folder)
+    model = train.fit_cnn_model(X_train, Y_train, X_test, Y_test, output_directory)
+
 
 if __name__ == "__main__":
     main()
--- src/train.py
+++ src/train.py
@@ -4,12 +4,14 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Conv1D, MaxPooling1D, Flatten
 from tensorflow.keras.optimizers import Adam
 import utils
+from datetime import datetime
 
-def fit_cnn_model(X_train, Y_train, X_test, Y_test, savedir, combination, num_layers=2,
+def fit_cnn_model(X_train, Y_train, X_test, Y_test, savedir, num_layers=2,
                   num_filters=64, kernel_size=3, pool_size=2, dropout_rate=0.2, learning_rate=0.001,
                   n_epochs=50, batch_size=32, verbose=1):
 
     model = Sequential()
+    date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     for _ in range(num_layers):
         model.add(Conv1D(filters=num_filters, kernel_size=kernel_size, activation='relu', padding='same'))
@@ -18,9 +20,9 @@ def fit_cnn_model(X_train, Y_train, X_test, Y_test, savedir, combination, num_la
 
     model.add(Flatten())
     model.add(Dense(64, activation='relu'))
-    model.add(Dense(Y_train.shape[1], activation='softmax'))
+    model.add(Dense(Y_train.shape[0], activation='softmax'))
 
-    opt = Adam(lr=learning_rate)
+    opt = Adam(learning_rate=learning_rate)
     model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 
     history = model.fit(X_train, Y_train,
@@ -35,24 +37,6 @@ def fit_cnn_model(X_train, Y_train, X_test, Y_test, savedir, combination, num_la
     print("Loss: %.2f%%" % (scores[0]))
     print("Accuracy: %.2f%%" % (scores[1] * 100))
 
-    plt.figure(figsize=(10, 10))
-    plt.plot(history.history['accurac'])
-    plt.plot(history.history['val_accuracy'])
-    plt.title('Model Accuracy')
-    plt.ylabel('Accuracy')
-    plt.xlabel('Epoch')
-    plt.legend(['Train', 'Validation'], loc='best')
-    plt.savefig(os.path.join(savedir, 'model_accuracy_' + combination + '.png'))
-
-    plt.figure(figsize=(10, 10))
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('Model Los')
-    plt.ylabel('Loss')
-    plt.xlabel('Epoch')
-    plt.legend(['Train', 'Validation'], loc='best')
-    plt.savefig(os.path.join(savedir, 'model_loss_' + combination + '.png'))
-
-    utils.save_model(model, os.path.join(savedir, 'savd_model_' + combination))
+    utils.save_model(model, os.path.join(savedir, 'savd_model_' + date_time))
 
     return model
